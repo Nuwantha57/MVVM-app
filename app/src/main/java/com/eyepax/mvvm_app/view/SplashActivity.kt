c4ad833984099ea7d13613c30825604941828788
Nuwantha Karunarathna <nuwantha.k@eyepax.com>
2025-11-20 12:07:09 +0530

    splash screen with MVVM

---
+++ app/src/main/java/com/eyepax/mvvm_app/view/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.eyepax.mvvm_app.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.eyepax.mvvm_app.R
+import com.eyepax.mvvm_app.viewmodel.SplashViewModel
+
+class SplashActivity : AppCompatActivity() {
+    private val splashViewModel: SplashViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        splashViewModel.isFinished.observe(this) { finished ->
+            if (finished) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }
+        splashViewModel.startTimer()
+    }
+}
